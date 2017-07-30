6b09b2e1c4695724735d81422cad0d61e7cc3f64
Gabrielle Simard-Moore <gsimardmoore@gmail.com>
2017-07-30 11:30:43 -0700

    Organize media models into media.py in models

--- kyc/models/categories.py
+++ kyc/models/categories.py
@@ -41,38 +41,3 @@ class Story(models.Model):
         super().save(*args, **kwargs)
     # author = models.ForeignKey(related_name='stories')
 
-
-def handler(instance, filename):
-    # TODO: complete filepath
-    filepath = f'{filename}'
-    return filepath
-
-
-class Media(models.Model):
-    '''
-    Used to upload multiple media objects to an object.
-    '''
-    file = models.FileField(upload_to=handler)
-    created = models.DateTimeField(editable=False)
-
-    class Meta:
-        abstract = True
-
-    def save(self, *args, **kwargs):
-        '''
-        Populate the created_date field, only when being created
-        '''
-        if not self.id:
-            self.created = timezone.now()
-
-        # TODO: extension = self.file.path.split('.')[-1]
-        # if extension in extensions ...
-
-        super().save(*args, **kwargs)
-
-
-class StoryMedia(Media):
-    story = models.ForeignKey(Story, related_name='story_media')
-
-class CategoryMedia(Media):
-    category = models.ForeignKey(Category, related_name='media_files')

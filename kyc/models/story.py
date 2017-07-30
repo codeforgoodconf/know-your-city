63d8f492c7d69fdbfdffe30ee9529b06613a0728
medullaskyline <kelly.e.iverson@gmail.com>
2017-07-30 11:17:30 -0700

    story model changes to override save

--- kyc/models/story.py
+++ kyc/models/story.py
@@ -5,8 +5,14 @@ class Story(models.Model):
     title = models.CharField(max_length=256, unique=True)
     # place = models.ForeignKey(Place)
 
-    date = models.DateField()
+    date = models.DateField()  # if this story takes place over a range of time, the date field will act as a start date
+    day_is_relevant = models.BooleanField(default=True)  # if the story started (and possibly ended) on an exact day
+    month_is_relevant = models.BooleanField(default=True)  # if the story started (and possibly ended on an exact month
+
     end_date = models.DateField(null=True)
+    end_day_is_relevant = models.NullBooleanField(null=True)  # if the story ended on an exact day
+    end_month_is_relevant = models.NullBooleanField(null=True)  # if the story ended on an exact month
+
 
     summary = models.CharField()
 
@@ -16,4 +22,16 @@ class Story(models.Model):
 
     references = models.CharField()
 
-    # author = ForeignKey(Author)
+    # author = models.ForeignKey(Author)
+    # theme = models.ForeignKey(Theme)
+
+    def save(self, commit=True):
+        story = super().save(commit=False)
+        if story.end_date is None:
+            story.end_day_is_relevant = None
+            story.end_month_is_relevant = None
+        if commit:
+            story.save()
+        return story
+
+

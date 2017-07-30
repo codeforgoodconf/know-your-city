ff47183dcedcd99838c72f1a2ab3ebc5279a166e
George Song <george@monozuku.com>
2017-07-30 08:00:49 -0700

    Add initial model file

---
+++ kyc/models/story.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Story(models.Model):
+    title = models.CharField(max_length=256, unique=True)
+    # place = models.ForeignKey(Place)
+
+    year = models.PositiveSmallIntegerField()
+    month = models.PositiveSmallIntegerField(null=True)
+    day = models.PositiveSmallIntegerField(null=True)
+
+    summary = models.CharField()
